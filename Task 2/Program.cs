078ae086639061470baf33d2a286d59ae2469d29
Alexey Sizov <alexsizov13@gmail.com>
2023-06-24 11:20:45 +0300

    added Task2 solution and Task3 description

---
+++ Task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int NumLenght(int num)
+{
+    int index = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        index++;
+    }
+    return index;
+}
+
+int Sum(int num, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int lenght = NumLenght(number);
+Console.WriteLine($"Суума цифр в введенном числе: {Sum(number, lenght)}");
+
